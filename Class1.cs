6a70ee38dde87c725d1c1bd22ccfa7d8a2ea17a5
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-12-21 20:17:12 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string ReverseWords(string str)
+    {
+        return string.Join(" ", str.Split(' ').Select(i => new string(i.Reverse().ToArray())));
+    }
+}
